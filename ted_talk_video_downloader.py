6fe3c6cd3963842d2c95c7cfaff9c694aad2c5ae
zamadshakil <146313727+zamadshakil@users.noreply.github.com>
2025-03-15 17:37:03 +0500

    some error handled

--- ted_talk_video_downloader.py
+++ ted_talk_video_downloader.py
@@ -6,13 +6,17 @@ import sys
 #for regular expression
 import re
 
-#error handling 
 
-if len(sys.argv > 1):
+#url: "https://ted.com/talks/ken_robinson_says_schools_kill_creativity"
+#error handling 
+'''
+if len(sys.argv) > 1:
     url = sys.argv[1]
 else:
     sys.exit("Error: Please enter url!")
-
+'''
+print("Process started...")
+url = "https://ted.com/talks/ken_robinson_says_schools_kill_creativity"
 response = requests.get(url)
 content = response.content
 
@@ -22,7 +26,7 @@ for val in soup.findAll("script"):
     if(re.search("talkPage.init", str(val))) is not None:
         result = str(val)
 
-result_mp4 = re.search("(?P<url>https?://[^\s]+)(mp4)", result).group("url")
+result_mp4 = re.search("(?P<url>https?://[^\\s]+)(mp4)", result).group("url")
 
 mp4_url = result_mp4.split('"')[0]
 
